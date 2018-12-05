cea1001359a6472d56d0c2f7a9d88e3334d2cc19
lab-ubuntu <packyande@gmail.com>
2018-12-05 13:20:20 +0800

    fixed file name

--- kitti2coco-label-trans.py
+++ kitti2coco-label-trans.py
@@ -3,11 +3,16 @@ import cv2
 import os
 import sys
 
+#data_set path
 kitti_img_path = '/home/pakcy/Desktop/PyTorch-YOLOv3/data/kitti/images/train/'
 kitti_label_path = '/home/pakcy/Desktop/PyTorch-YOLOv3/data/kitti/raw_labels/train/'
+
+#transformed lables path
 kitti_label_tosave_path = 'kitti/labels2coco/'
 
+#the real ptah of your data set
 kitti_data_real_path = '/home/pakcy/Desktop/PyTorch-YOLOv3/data/kitti/images/train/'
+
 index = 0
 cvfont = cv2.FONT_HERSHEY_SIMPLEX
 
@@ -75,10 +80,10 @@ for indexi in range(len(kitti_images)):
                 #print(kitti_names_contents[class_num])
                 # cv2.putText()
                 # 输入参数为图像、文本、位置、字体、大小、颜色数组、粗细
-                cv2.putText(kitti_img_totest, class_str, (intx1, inty1+3), cvfont, 2, (0,0,255), 1)
+                #cv2.putText(kitti_img_totest, class_str, (intx1, inty1+3), cvfont, 2, (0,0,255), 1)
                 # cv2.rectangle()
                 # 输入参数分别为图像、左上角坐标、右下角坐标、颜色数组、粗细
-                cv2.rectangle(kitti_img_totest, (intx1,inty1), (intx2,inty2), (0,255,0), 2)
+                #cv2.rectangle(kitti_img_totest, (intx1,inty1), (intx2,inty2), (0,255,0), 2)
                 line_to_write = str(kitti_names_num[class_str]) + ' ' + str(bbox_center_x)+ ' ' + str(bbox_center_y)+ ' ' + str(bbox_width)+ ' ' + str(bbox_height) +'\n'
                 real_label.write(line_to_write)
                 sys.stdout.write(str(int((indexi/len(kitti_images))*100))+'% '+'*******************->' "\r" )
